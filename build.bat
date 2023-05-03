98d0aa4b5630d2a602979a4566f67eb3b16a7d0c
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-05-03 20:13:55 +0200

    refactor: command arguments

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 title GMail-Bomber Builder
-pyinstaller --icon=res/icon.ico --onefile --noconsole gmail_bomber.py
+pyinstaller --icon=res/icon.ico --onefile --noconsole ./gmail_bomber.py
 rmdir /s /q "./build/"
 ren dist build
 del "*.spec"
